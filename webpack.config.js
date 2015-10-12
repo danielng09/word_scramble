3a4dda72046e9d12a72aaa5bf0828fb9881aa730
Daniel Ng <dvng13@gmail.com>
2015-10-11 23:34:04 -0700

    edit for production

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
 		app: ['./src/js/index.js']
 	},
 	output: {
-    path: path.resolve('build'),
-    publicPath: '/',
+    path: path.join(__dirname),
     filename: 'bundle.js'
   },
 
